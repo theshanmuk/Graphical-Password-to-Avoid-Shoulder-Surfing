837465e1603443a0c78d023e4c0a08f694a340a2
mr_shanmuk <shanmukmichael@gmail.com>
2020-08-23 10:00:06 +0530

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()

6b238f4cabbf5d3a8de9986f77388274b827882e
mr_shanmuk <shanmukmichael@gmail.com>
2020-08-23 08:28:49 +0530

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -7,11 +7,10 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = 'cybersecurity'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
     db.init_app(app)
 
